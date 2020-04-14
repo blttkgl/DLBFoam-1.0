29e21095117ef6e7d61d52417cd95ecf7f564c4c
peltonp1 <peltonp1@aalto.fi>
2020-04-14 15:50:35 +0300

    first test

--- unittests/test.C
+++ unittests/test.C
@@ -1,6 +1,10 @@
 // This tells Catch to provide a main() - only do this in one cpp file (per
 // binary)
+
 #define CATCH_CONFIG_MAIN
 #define MATH_DEBUG 1
 
-#include "./third_party/catch/catch.hpp"
+#include "../third_party/catch.hpp"
+
+
+
---
+++ unittests/testRefMapper.C
@@ -0,0 +1,9 @@
+#include "../third_party/catch.hpp"
+
+
+
+TEST_CASE("refmapper constructors"){
+
+    CHECK(1 == 2);
+
+}
