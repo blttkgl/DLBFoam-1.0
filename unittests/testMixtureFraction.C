043edcf42d6eceb816b1fc7a8306ba31eb31a186
peltonp1 <peltonp1@aalto.fi>
2020-04-14 17:04:30 +0300

    added example unittests

--- unittests/testMixtureFraction.C
+++ unittests/testMixtureFraction.C
@@ -9,6 +9,15 @@ TEST_CASE("mixtureFraction constructors"){
 
     REQUIRE_NOTHROW(mixtureFraction());
     
+    dictionary mix_frac_dict;
+    mix_frac_dict.add("asd", 3);
+    wordList species;
 
+    REQUIRE_NOTHROW(mixtureFraction(mix_frac_dict, species));
+
+
+
+
+    //const dictionary& mixFracDict, const wordList& species
 
 }
