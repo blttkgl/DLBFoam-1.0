336d7612d1f8483960aaaf04abd884343a63033e
Tekgül Bulut <tekgulb1@t20204-lr010.org.aalto.fi>
2020-09-01 17:05:08 +0300

    Added cluster scripts here for easier access TRITON/MAHTI

---
+++ benchmarks/cases/small/runMahti.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=example
+#SBATCH --account=XXX
+#SBATCH --partition=test
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+
+. /appl/soft/eng/OpenFOAM/OpenFOAM-8/etc/bashrc
+
+# Run all clean
+./Allclean
+
+# Get the processor count
+N_PROCS=$(nproc)
+
+# Set the processor count on blockMeshDict and decomposeParDict
+foamDictionary system/decomposeParDict -entry nProc -set $N_PROCS
+foamDictionary system/decomposeParDict -entry nProc -set $N_PROCS
+
+# Set cells per core for benchmark
+CELLS_PER_CORE=20
+foamDictionary system/decomposeParDict -entry cellsPerCore -set $CELLS_PER_CORE
+
+# Blockmesh, decomposePar
+blockMesh > log.blockMesh
+decomposePar > log.decompose
+
+# Run the benchmark
+srun chemistryBenchmark -parallel > log.chemistryBenchmark
+
+
+
+
---
+++ benchmarks/cases/small/runTriton.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -t 00:30:00
+#SBATCH -n 4
+
+#set -euo pipefail
+
+. /share/apps/spack/envs/fgci-centos7-generic/software/openfoam-org/8/fadnomj/etc/bashrc
+module load gcc
+
+# Run all clean
+./Allclean
+
+# Get the processor count
+N_PROCS=$(nproc)
+
+# Set the processor count on blockMeshDict and decomposeParDict
+foamDictionary system/decomposeParDict -entry nProc -set $N_PROCS
+foamDictionary system/decomposeParDict -entry nProc -set $N_PROCS
+
+# Set cells per core for benchmark
+CELLS_PER_CORE=20
+foamDictionary system/decomposeParDict -entry cellsPerCore -set $CELLS_PER_CORE
+
+# Blockmesh, decomposePar
+blockMesh > log.blockMesh
+decomposePar > log.decompose
+
+# Run the benchmark
+srun chemistryBenchmark -parallel > log.chemistryBenchmark
+
+
